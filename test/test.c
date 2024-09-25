bf955f8346eae8896b6a95ff3f2f73ca670fb922
Alex <alexandercromar@gmail.com>
2024-09-25 11:20:00 -0600

    added first test updateCounter

--- test/test.c
+++ test/test.c
@@ -3,33 +3,34 @@
 #include <stdint.h>
 #include <unity.h>
 #include "unity_config.h"
+#include "threads_helper.h"
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
+/**\
+ * This test creates a counter and semaphore and makes sure it updates counter.
+ */
+void test_updateCounter_runs()
 {
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+   SemaphoreHandle_t semaphore = xSemaphoreCreateCounting(1, 1);
+   int count = 0;
+   int result = updateCounter(1,&count,semaphore);
+    //test that count happened and the count was updated.
+   TEST_ASSERT_EQUAL_INT16_MESSAGE(pdTRUE,result, "updateCounter did not run when it should have");
+   TEST_ASSERT_EQUAL_INT16_MESSAGE(1,count,"the counter did not update itself.");
+
 }
 
-void test_multiplication(void)
-{
-    int x = 30;
-    int y = 6;
-    int z = x / y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 5, "Multiplication of two integers returned incorrect value.");
-}
 
 int main (void)
 {
     stdio_init_all();
-    sleep_ms(5000); // Give time for TTY to attach.
+    sleep_ms(10000); // Give time for TTY to attach.
     printf("Start tests\n");
     UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
+    RUN_TEST(test_updateCounter_runs);
     sleep_ms(5000);
     return UNITY_END();
 }
